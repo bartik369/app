f764d048b121e9669ec938fc36a8d31fe49895d0
zzc0de <6apmuk@gmail.com>
2022-04-17 23:19:24 +0300

    1

--- server/index.js
+++ server/index.js
@@ -2,40 +2,20 @@ const express = require('express')
 const mongoose = require('mongoose');
 const app = express()
 const cors = require('cors');
-const Device = require('./DB/deviceScheme')
-
 // MONGO SCHEME
-
-
 
 app.use(express.json());
 app.use(cors());
 
-app.get('/', (req, resp) => {
-    resp.send('The App is working');
-});
-
-mongoose.connect('mongodb+srv://zzc0de:PfrhsnsqL0cneg@digitalcluster.chauh.mongodb.net/myFirstDatabase?retryWrites=true&w=majority', {
-        dbName: '',
-        useNewUrlParser: true,
-        useUnifiedTopology: true,
-    }, error => error ?
-    console.log(error) :
-    console.log('Connected to DB')
-)
-
-app.post('/register', async(req, resp) => {
-    try {
-        const device = new Device(req.body);
-        let result = await device.save();
-        result = result.toObject()
-
-        if (result) {
-            resp.send(req.body);
-        }
-    } catch (error) {
-        resp.send('Something get wrong')
-    }
+mongoose.connect('mongodb+srv://zzc0de:PfrhsnsqL0cneg@digitalcluster.chauh.mongodb.net/devices?retryWrites=true&w=majority', {
+    useNewUrlParser: true,
 })
 
-app.listen(5001, () => console.log('The server was started on 5000 port'))
+const start = async() => {
+    try {
+        app.listen(5001, () => console.log('The server was started on 50001 port'))
+    } catch (error) {
+        console.log(error)
+    }
+}
+start()
