d67ae0f27ec7fdfa7edb782665d6989cd3a98d4c
zzc0de <6apmuk@gmail.com>
2022-04-23 22:48:09 +0300

    3

--- client/src/components/sidebar/SideBar.jsx
+++ client/src/components/sidebar/SideBar.jsx
@@ -4,10 +4,10 @@ import MenuItem from './MenuItem';
 const Sidebar = (props) => {
   const [inActive, setInactive] = useState(false);
   const menuItem = [
-    {name: 'Главная', to: '/', iconClassName: 'bi bi-house-door'},
-    {name: 'Статистика', to: '/statistic', iconClassName: 'bi bi-bar-chart'},
-    {name: 'Пользователи', to: '/users', iconClassName: 'bi bi-people'},
-    {name: 'Задачи', to: 'Tasks', iconClassName: 'bi bi-check2-square'},
+    {name: 'Главная', to: '/', iconClassName: 'bi bi-house-fill'},
+    {name: 'Статистика', to: '/statistic', iconClassName: 'bi bi-bar-chart-fill'},
+    {name: 'Пользователи', to: '/users', iconClassName: 'bi bi-people-fill'},
+    {name: 'Задачи', to: 'Tasks', iconClassName: 'bi bi-file-earmark-check-fill'},
   ]
 
   return (
@@ -21,9 +21,9 @@ const Sidebar = (props) => {
           className="toggle-menu-btn"
         >
           {inActive ? (
-            <i class="bi bi-arrow-left-square-fill"></i>
+            <i class="bi bi-arrow-left"></i>
           ) : (
-            <i class="bi bi-arrow-right-square-fill"></i>
+            <i class="bi bi-arrow-right"></i>
           )}
         </button>
       </div>
@@ -37,39 +37,6 @@ const Sidebar = (props) => {
             to={item.to}
              />
           ))}
-          {/* <li className="menu__item">
-            <div className="icon">
-              <i class="bi bi-house-door"></i>
-            </div>
-            <a href="" className="menu__link">
-              Главная
-            </a>
-          </li>
-          <li className="menu__item">
-            <div className="icon">
-              <i class="bi bi-bar-chart"></i>
-            </div>
-            <a href="" className="menu__link">
-              Статистика
-            </a>
-          </li>
-          <li className="menu__item">
-            <div className="icon">
-              <i class="bi bi-people"></i>
-            </div>
-            <a href="" className="menu__link">
-              Пользователи
-            </a>
-          </li>
-          <li className="menu__item">
-            <div className="icon">
-              <i class="bi bi-check2-square"></i>
-            </div>
-            <a href="" className="menu__link">
-              Задачи
-            </a>
-          </li>
-          <li className="menu__item"></li> */}
         </ul>
       </nav>
     </div>
