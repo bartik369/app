431b362ad9f690a8c51763da44110b466fe0ef64
zzc0de <6apmuk@gmail.com>
2022-04-26 22:29:46 +0300

    good

--- client/src/components/UI/search/SearchData.jsx
+++ client/src/components/UI/search/SearchData.jsx
@@ -2,16 +2,10 @@ import React, {useState} from "react";
 import classes from './SearchData.module.css'
 
 const SearchData = (props) => {
-    const [searchQuery, setSearchQuery] = useState('');
 
     return (
         <div className={classes.searchInput}>
-            <input 
-            onChange={(e) => setSearchQuery(e.target.value)}
-            type="text" 
-            value={searchQuery}
-            placeholder="Поиск..." />
-            <div className={classes.searchBtn}><i class="bi bi-search"></i></div>
+            <input {...props} />
         </div>
     )
 }
--- client/src/components/pages/DeviceSearch.jsx
+++ client/src/components/pages/DeviceSearch.jsx
@@ -1,37 +1,49 @@
-import React, {useEffect, useState} from "react";
-import Axios from 'axios';
+import React, { useEffect, useMemo, useState } from "react";
+import Axios from "axios";
 import DeviceLists from "../DeviceLists";
 import SearchData from "../UI/search/SearchData";
 
 const DeviceSearch = () => {
+  const [devices, setDevices] = useState([
+    {
+      id: "",
+      deviceType: "",
+      deviceName: "",
+      deviceNumber: "",
+      userName: "",
+      deviceAddTime: "",
+    },
+  ]);
 
-    const [devices, setDevices] = useState([
-        {
-          id: '',
-          deviceType: '',
-          deviceName: '',
-          deviceNumber: '',
-          userName: '',
-          deviceAddTime: '',
-        },
-      ]);
+  const [searchQuery, setSearchQuery] = useState("");
 
-    useEffect(() => {
-        Axios.get('http://localhost:5001/read').then((response) => {
-          setDevices(response.data)
-        });
-      }, []);
+  useEffect(() => {
+    Axios.get("http://localhost:5001/read").then((response) => {
+      setDevices(response.data);
+    });
+  }, []);
 
-      function removeDevice(id) {
-        Axios.delete(`http://localhost:5001/delete/${id}`)
-      }
+  function removeDevice(id) {
+    Axios.delete(`http://localhost:5001/delete/${id}`);
+  }
 
-    return (
-        <div className="device-search">
-            <SearchData/>
-            <DeviceLists remove={removeDevice} title="Devices" devices={devices} />
-        </div>
-    )
-}
+  const sortedAndSearchDevices = useMemo(() => {
+    return devices.filter(device => device.deviceNumber.includes(searchQuery))
+  }, [searchQuery, devices]);
+
+  return ( 
+    <div className="device-search">
+      <SearchData
+        placeholder='Поиск...'
+        onChange={e => setSearchQuery(e.target.value)}
+      />
+      <DeviceLists
+        remove={removeDevice}
+        title="Devices"
+        devices={sortedAndSearchDevices}
+      />
+    </div>
+  );
+};
 
 export default DeviceSearch;
