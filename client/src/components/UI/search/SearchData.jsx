147e3953b99a5366da664dffdea5deca60d6d865
zzc0de <6apmuk@gmail.com>
2022-05-14 16:40:04 +0300

    1

--- client/src/components/UI/search/SearchData.jsx
+++ client/src/components/UI/search/SearchData.jsx
@@ -1,17 +1,20 @@
 import React from "react";
 import './SearchData.css'
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
-import { faXmark} from '@fortawesome/free-solid-svg-icons';
+import { faMagnifyingGlass, faXmark} from '@fortawesome/free-solid-svg-icons';
 
 const SearchData = (props) => {
 
     return (
-        <div className="search-input">
-            <input {...props} />
+        <div className="search-box">
+            <button className="btn-search">
+                <FontAwesomeIcon icon={faMagnifyingGlass}/>
+            </button>
             <FontAwesomeIcon 
             icon={faXmark} 
             className={`delete-searchquery ${props.searchQueryLength <= 0 ? "" : "active"}`} 
             onClick={props.delSearchQuery}/>
+            <input {...props} className="input-search"/>
         </div>
     )
 }
