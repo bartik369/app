f7f732e05f6828507e239564e8cb1ffec5cde688
zzc0de <6apmuk@gmail.com>
2022-05-25 22:53:18 +0300

    back todo schemes

---
+++ server/models/Todo.js
@@ -0,0 +1,22 @@
+import mongoose from "mongoose";
+
+const ToDoScheme = new mongoose.Schema({
+    toDoTitle: {
+        type: String,
+        required: true,
+    },
+    toDoDescription: {
+        type: String,
+        required: true,
+    },
+    toDoAddTime: {
+        type: String,
+    },
+    toDoDeadline: {
+        type: String,
+    },
+});
+
+const ToDo = mongoose.model("TodoData", ToDoScheme);
+
+export default ToDo
