78d8eb762d7591bdd11c959ac214616ac30969e2
zzc0de <6apmuk@gmail.com>
2022-05-29 14:30:20 +0300

    css todo

--- client/src/components/pages/Todos.jsx
+++ client/src/components/pages/Todos.jsx
@@ -2,6 +2,11 @@ import React, { useState } from "react";
 import AddTodoForm from "../form/AddTodoForm";
 import Axios from "axios";
 import ENV from "../../env.config";
+import './Todos.css'
+import Modal from "../UI/modal/Modal";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faPenToSquare, faSquareCheck, faTrashCan } from "@fortawesome/free-solid-svg-icons";
+
 
 const Todos = ({todos}) => {
   const createToDo = (todoData) => {
@@ -15,18 +20,41 @@ const Todos = ({todos}) => {
   };
 
   return (
-    <div className="tasks">
-      <AddTodoForm create={createToDo} />
-      <div>
+    <div className="todos">
+      <div className="todo-list">
       {todos.map((todo, index) => {
           return (
-            <div className="todo-list" key={index}>
-            <div>{todo.todoTitle}</div>
-            <div>{todo.todoDescription}</div>
-            <div>{todo.todoAddTime}</div>
+            <div className="todo-item" key={index}>
+            <div className="todo-item__title">{todo.todoTitle}</div>
+            <div className="todo-item__description">{todo.todoDescription}</div>
+            <div className="todo-item__addtime">{todo.todoAddTime}</div>
+            <div className="todo-btns">
+              <ul className="todo-btns__inner">
+                <li className="todo-btns__item">
+                  <button onClick={() => console.log('end')} className="todoend-btn">
+                  <FontAwesomeIcon icon={faSquareCheck} />
+                  </button>
+                </li>
+                <li className="todo-btns__item">
+                  <button onClick={() => console.log('update')} className="todoupdate-btn">
+                  <FontAwesomeIcon icon={faPenToSquare} />
+                  </button>
+                </li>
+                <li className="todo-btns__item">
+                  <button onClick={() => console.log('delete')} className="tododel-btn">
+                  <FontAwesomeIcon icon={faTrashCan} />
+                  </button>
+                </li>
+              </ul>
+            </div>
           </div>
           )
       })}
+      </div>
+      <div className="add-todo">
+      <Modal>
+      <AddTodoForm create={createToDo} />
+      </Modal>
       </div>
     </div>
   );
