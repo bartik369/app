82ff5944fd6eab76efcb37767e8c3bf5cb6804bf
zzc0de <6apmuk@gmail.com>
2022-05-29 15:06:30 +0300

    2

--- client/src/components/pages/Todos.jsx
+++ client/src/components/pages/Todos.jsx
@@ -2,13 +2,16 @@ import React, { useState } from "react";
 import AddTodoForm from "../form/AddTodoForm";
 import Axios from "axios";
 import ENV from "../../env.config";
-import './Todos.css'
+import "./Todos.css";
 import Modal from "../UI/modal/Modal";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
-import { faPenToSquare, faSquareCheck, faTrashCan } from "@fortawesome/free-solid-svg-icons";
+import {
+  faPenToSquare,
+  faSquareCheck,
+  faTrashCan,
+} from "@fortawesome/free-solid-svg-icons";
 
-
-const Todos = ({todos}) => {
+const Todos = ({ todos }) => {
   const createToDo = (todoData) => {
     const { todotitle, tododescription, todoAddTime } = todoData;
 
@@ -22,39 +25,54 @@ const Todos = ({todos}) => {
   return (
     <div className="todos">
       <div className="todo-list">
-      {todos.map((todo, index) => {
+        {todos.map((todo, index) => {
           return (
             <div className="todo-item" key={index}>
-            <div className="todo-item__title">{todo.todoTitle}</div>
-            <div className="todo-item__description">{todo.todoDescription}</div>
-            <div className="todo-item__addtime">{todo.todoAddTime}</div>
-            <div className="todo-btns">
-              <ul className="todo-btns__inner">
-                <li className="todo-btns__item">
-                  <button onClick={() => console.log('end')} className="todoend-btn">
-                  <FontAwesomeIcon icon={faSquareCheck} />
-                  </button>
-                </li>
-                <li className="todo-btns__item">
-                  <button onClick={() => console.log('update')} className="todoupdate-btn">
-                  <FontAwesomeIcon icon={faPenToSquare} />
-                  </button>
-                </li>
-                <li className="todo-btns__item">
-                  <button onClick={() => console.log('delete')} className="tododel-btn">
-                  <FontAwesomeIcon icon={faTrashCan} />
-                  </button>
-                </li>
-              </ul>
+              <div className="todo-item__title">{todo.todoTitle}</div>
+              <div className="todo-item__description">
+                {todo.todoDescription}
+              </div>
+              <hr className="separate"/>
+              <div className="time-info">
+                <div className="todo-item__addtime">{todo.todoAddTime}</div>
+                <div className="todo-item__deadline">5/28/2022 22:39:12</div>
+              </div>
+              <div className="todo-btns">
+                <ul className="todo-btns__inner">
+                  <li className="todo-btns__item">
+                    <button
+                      onClick={() => console.log("end")}
+                      className="todoend-btn"
+                    >
+                      <FontAwesomeIcon icon={faSquareCheck} />
+                    </button>
+                  </li>
+                  <li className="todo-btns__item">
+                    <button
+                      onClick={() => console.log("update")}
+                      className="todoupdate-btn"
+                    >
+                      <FontAwesomeIcon icon={faPenToSquare} />
+                    </button>
+                  </li>
+                  <li className="todo-btns__item">
+                    <button
+                      onClick={() => console.log("delete")}
+                      className="tododel-btn"
+                    >
+                      <FontAwesomeIcon icon={faTrashCan} />
+                    </button>
+                  </li>
+                </ul>
+              </div>
             </div>
-          </div>
-          )
-      })}
+          );
+        })}
       </div>
       <div className="add-todo">
-      <Modal>
-      <AddTodoForm create={createToDo} />
-      </Modal>
+        <Modal>
+          <AddTodoForm create={createToDo} />
+        </Modal>
       </div>
     </div>
   );
