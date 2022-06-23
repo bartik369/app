858c96b8e9448eb6f3bc679f067ac770c79be120
zzc0de <6apmuk@gmail.com>
2022-06-23 23:39:16 +0300

    configuring form validation

--- client/src/components/form/AddTodoForm.jsx
+++ client/src/components/form/AddTodoForm.jsx
@@ -1,4 +1,4 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 import FormInput from "./FormInput";
 import DatePicker from "react-datepicker";
 import "react-datepicker/dist/react-datepicker.css";
@@ -6,7 +6,6 @@ import ru from 'date-fns/locale/ru';
 
 
 const AddTodoForm = ({ create, modal }) => {
-
   const [todo, setTodo] = useState(
     {
       id: "",
@@ -17,6 +16,77 @@ const AddTodoForm = ({ create, modal }) => {
       endTime: "",
     }
   );
+
+  const [titleDirty, setTitleDirty] = useState(false);
+  const [descriptionDirty, setDescriptionDirty] = useState(false);
+  const [startTimeDirty, setStartTimeDirty] = useState(false);
+  const [endTimeDirty, setEndTimeDirty] = useState(false);
+
+  const [titleError, setTitleError] = useState('Укажите название');
+  const [descriptionError, setDescriptionError] = useState('Укажите описание');
+  const [startTimeError, setStartTimeError] = useState('Укажите дату начала');
+  const [endTimeError, setEndTimeError] = useState('Укажите дату окончания');
+  const [validForm, setValidForm] = useState(false);
+
+  useEffect(() => {
+    if (titleError || descriptionError || startTimeError || endTimeError) {
+      setValidForm(false)
+    } else {
+      setValidForm(true)
+    }
+
+  }, [titleError, descriptionError, startTimeError, endTimeError]);
+
+
+  const bluerHandler = (e) => {
+    switch (e.target.name) {
+      case 'title':
+        setTitleDirty(true)
+        break
+      case 'description':
+        setDescriptionDirty(true)
+        break
+      case 'starttime':
+        setStartTimeDirty(true)
+        break
+      case 'endtime':
+        setEndTimeDirty(true)
+        break
+    }
+
+  }
+
+  const titleHandler = (e) => {
+    setTodo({...todo, title: e.target.value});
+    setTitleError('');
+    if (!e.target.value) {
+      setTitleError('Укажите название')
+    }
+  }
+
+  const descriptionHandler = (e) => {
+    setTodo({...todo, description: e.target.value})
+    setDescriptionError('');
+    if (!e.target.value) {
+      setDescriptionError('Укажите описание')
+    }
+  }
+
+  const startTimeHandler = (date) => {
+    setTodo({...todo, startTime:date})
+    setStartTimeError('');
+    if (!date.target.value) {
+      setStartTimeError('Укажите дату начала')
+    }
+  }
+  const endTimeHandler = (date) => {
+    setTodo({...todo, endTime:date})
+    setEndTimeError('');
+    if (!date.target.value) {
+      setEndTimeError('Укажите дату конца')
+    }
+  }
+
 
   const addTodoHandler = () => {
      const newTodo = {
@@ -44,22 +114,29 @@ const AddTodoForm = ({ create, modal }) => {
 
   return (
     <div className="add-todo-form">
+      {(titleDirty && titleError) && <div>{titleError}</div>}
       <FormInput
+        onBlur={e => bluerHandler(e)}
         placeholder="Название задачи"
         value={todo.title}
         name="title"
-        onChange={(e) => setTodo({...todo, title: e.target.value})}
+        onChange={(e) => titleHandler(e)}
       />
+      {(descriptionDirty && descriptionError) && <div>{descriptionError}</div>}
       <textarea
+        onBlur={e => bluerHandler(e)}
         value={todo.description}
         name="description"
-        onChange={(e) => setTodo({...todo, description: e.target.value})}
+        onChange={(e) => descriptionHandler(e)}
         cols="20"
         rows="10"
       />
+      {(startTimeDirty && startTimeError) && <div>{startTimeError}</div>}
       <DatePicker
+        onBlur={e => bluerHandler(e)}
+        name="starttime"
         selected={todo.startTime}
-        onChange={(date) => setTodo({...todo, startTime:date})}
+        onChange={(date) => startTimeHandler(date)}
         selectsStart
         startDate={todo.startTime}
         endDate={todo.endTime}
@@ -72,9 +149,12 @@ const AddTodoForm = ({ create, modal }) => {
         timeCaption="time"
         locale={ru}
       />
+       {(endTimeDirty && endTimeError) && <div>{endTimeError}</div>}
       <DatePicker
+        onBlur={e => bluerHandler(e)}
+        name="endtime"
         selected={todo.endTime}
-        onChange={(date) => setTodo({...todo, endTime:date})}
+        onChange={(date) => endTimeHandler(date)}
         selectsEnd
         startDate={todo.startTime}
         endDate={todo.endTime}
@@ -89,7 +169,7 @@ const AddTodoForm = ({ create, modal }) => {
         locale={ru}
       />
       
-      <button className="add-btn" onClick={() => addTodoHandler()}>
+      <button disabled={!validForm} type='submit' className="add-btn" onClick={() => addTodoHandler()}>
         Добавить
       </button>
     </div>
