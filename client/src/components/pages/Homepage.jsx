4be4bdfa0345289c043942e06122e5985a49eded
Alex Barth <6apmuk@gmail.com>
2022-07-11 17:32:03 +0300

    todo widjet in process

--- client/src/components/pages/Homepage.jsx
+++ client/src/components/pages/Homepage.jsx
@@ -4,15 +4,15 @@ import "../widgets/widgets.css";
 const Homepage = ({ devices, todos }) => {
   const arrayDevices = [...devices];
   const arrayTodos = [...todos]
+  const dateNow = new Date().toLocaleString('ru-RU');
   const reverseArrayDevices = arrayDevices.reverse().slice(0, 8);
-  const reverseArrayTodos = arrayTodos.reverse().slice(0, 5);
 
   return (
     <div className="home-wrapper">
       <div className="top-side">
         <div className="widget-item">
           <div className="widget-item__title">Последние добавленные</div>
-          <table className="last-devices-table">
+          <table className="widget-table">
               <thead>
                   <tr>
                       <th>Модель устройства</th>
@@ -34,13 +34,28 @@ const Homepage = ({ devices, todos }) => {
           </table>
         </div>
         <div className="widget-item">
-          <div className="widget-item__title">Последние задачи</div>
-          {reverseArrayTodos.map((todo, index) => (
-            <div key={index}>
-              {todo.title}
-              {todo.startTime}
-          </div>
-          ))}
+          <div className="widget-item__title">Срочно выполнить</div>
+          <table className="widget-table">
+              <thead>
+                  <tr>
+                      <th>Название</th>
+                      <th>Начать</th>
+                      <th>Закончить</th>
+                  </tr>
+              </thead>
+              <tbody>
+                  {arrayTodos.map((todo, index) => (
+                      todo.endTime <= dateNow
+                      ?
+                      <tr key={index}>
+                          <td>{todo.title}</td>
+                          <td>{todo.startTime}</td>
+                          <td>{todo.endTime}</td>
+                      </tr>
+                      : ""
+                  ))}
+              </tbody>
+          </table>
         </div>
       </div>
     </div>
