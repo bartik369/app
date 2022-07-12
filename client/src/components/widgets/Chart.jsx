7b4bc68a3e365ff159ab1db41293e97d40024764
Alex Barth <6apmuk@gmail.com>
2022-07-12 16:20:21 +0300

    2

--- client/src/components/widgets/Chart.jsx
+++ client/src/components/widgets/Chart.jsx
@@ -4,7 +4,7 @@ import "../widgets/widgets.css";
 const Chart = ({devices}) => {
     return (
         <div className="widget-item">
-            <div className="widget-item__title">Статисика</div>
+            <div className="widget-item__title">Статиcтика по оборудованию</div>
             {devices.map((item) => {
                 console.log(item)
             })}
@@ -13,3 +13,10 @@ const Chart = ({devices}) => {
 }
 
 export default Chart;
+
+
+// document.write('<pre>', JSON.stringify(
+//   [1, 3, 4, 1, 1, 3, 4, 5].reduce((acc, el) => {
+//     acc[el] = (acc[el] || 0) + 1;
+//     return acc;
+//   }, {}), null, 2), '</pre>');
