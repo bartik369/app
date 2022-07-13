71ba44fea00fcd2f9ea6a87ed9f92511b69a4dc3
zzc0de <6apmuk@gmail.com>
2022-07-13 23:28:16 +0300

    1

--- client/src/components/widgets/Chart.jsx
+++ client/src/components/widgets/Chart.jsx
@@ -2,13 +2,12 @@ import React from "react";
 import "../widgets/widgets.css";
 
 const Chart = ({devices}) => {
-    let deviceArray = [...devices]
     let newData = []
     let count = [];
     return (
         <div className="widget-item">
             <div className="widget-item__title">Статиcтика по оборудованию</div>
-            {deviceArray.map(function(item) {
+            {devices.map(function(item) {
                 newData.push(item.type)
                 newData.forEach(function(i) {
                     count[i] = (count[i]||0) + 1
