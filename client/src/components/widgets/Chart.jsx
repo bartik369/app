2d5585d50afee18264b933631ca72a958ca30226
Alex Barth <6apmuk@gmail.com>
2022-07-14 15:27:29 +0300

    1

--- client/src/components/widgets/Chart.jsx
+++ client/src/components/widgets/Chart.jsx
@@ -2,21 +2,24 @@ import React from "react";
 import "../widgets/widgets.css";
 
 const Chart = ({devices}) => {
-    let newData = []
-    let count = [];
+
+    const getDevicesCount = () => {
+        const nameArray = [];
+        devices.map((item) => {
+            nameArray.push(item.type);
+        });
+        console.log(nameArray);
+    }
+    getDevicesCount()
+
     return (
         <div className="widget-item">
             <div className="widget-item__title">Статиcтика по оборудованию</div>
-            {devices.map(function(item) {
-                newData.push(item.type)
-                newData.forEach(function(i) {
-                    count[i] = (count[i]||0) + 1
-                })
-                console.log(count)
-            })}
+            
         </div>
     )
 }
 
 export default Chart;
+
 
