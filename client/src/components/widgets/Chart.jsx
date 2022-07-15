8561ed5b0c8f971c1207cba11c636fe0bd4a9e6f
Alex Barth <6apmuk@gmail.com>
2022-07-15 16:47:02 +0300

    1

--- client/src/components/widgets/Chart.jsx
+++ client/src/components/widgets/Chart.jsx
@@ -6,17 +6,23 @@ const CanvasJSChart = CanvasJSReact.CanvasJSChart;
 const Chart = ({devices}) => {
 
     const nameArray = [];
-    const count = [];
+    let newArray = []
+    let count = []
     const getDevicesCount = () => {
         devices.map((item) => {
             nameArray.push(item.type);
         });
-        nameArray.forEach(function(x) {
-            count[x] = (count[x] || 0) + 1
+        nameArray.map((sum) => {
+            count[sum] = (count[sum] || 0) + 1
+            // count[x] = (count[x] || 0) + 1
         })
     }
+    
     getDevicesCount()
+
     console.log(count)
+    console.log(newArray)
+
 
     const options = {
         animationEnabled: true,
@@ -24,14 +30,14 @@ const Chart = ({devices}) => {
         data: [
             {
                 type: "bar",
-                dataPoints:[
-                    { y:  2200000000, label: "Facebook" },
-					{ y:  1800000000, label: "YouTube" },
-					{ y:  800000000, label: "Instagram" },
-					{ y:  563000000, label: "Qzone" },
-					{ y:  376000000, label: "Weibo" },
-					{ y:  336000000, label: "Twitter" },
-					{ y:  330000000, label: "Reddit" }
+                dataPoints:[...count
+                    // { y:  2200000000, label: "Facebook" },
+					// { y:  1800000000, label: "YouTube" },
+					// { y:  800000000, label: "Instagram" },
+					// { y:  563000000, label: "Qzone" },
+					// { y:  376000000, label: "Weibo" },
+					// { y:  336000000, label: "Twitter" },
+					// { y:  330000000, label: "Reddit" }
                 ]
             }
         ]
