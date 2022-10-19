e0f924c5287d34e44ac062c1f344dcf45ad0ded5
Alex Barth <6apmuk@gmail.com>
2022-10-19 16:11:13 +0300

    refactoring for react hook form

--- client/src/components/form/Login.jsx
+++ client/src/components/form/Login.jsx
@@ -3,52 +3,69 @@ import { Link } from "react-router-dom";
 import "./Login.css";
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { faEnvelope, faLock } from '@fortawesome/free-solid-svg-icons';
-import useSignup from '../../hooks/useSignup'
-import validateAuth from '../validate/validateAuth.js'
+import {useForm} from 'react-hook-form';
 
 export default function Login() {
 
-  const {signupHandler, signup, errors} = useSignup(validateAuth);
+  const {
+    register,
+    formState: {errors},
+    handleSubmit,
+  } = useForm();
 
+  const onSubmit = (data) => {
+    console.log(data)
+  }
 
   return (
     <div className="main">
       <div className="login">
        <div className="login-sidebar"></div>
-        <form className="login-form" action="" onSubmit={signup}>
-        <div className="login-form__title">Авторизация</div>
-            <label className="login-form__label" for="email">Почта</label>
-            <div className="login-form__input">
-            <FontAwesomeIcon icon={faEnvelope} className="input-icon" />
-            <input className={errors.email ? "validation-error" : ""}
-            type="text" 
-            id="email" 
-            name='email'
-            placeholder='Укажите свою почту'
-            onChange={signupHandler}
-            />
-            </div>
-             <div className="form-error">
-                {errors.email && <p>{errors.email}</p>}
-            </div>
-            <label className="login-form__label" for="password">Пароль</label>
-            <div className="login-form__input">
-            <FontAwesomeIcon icon={faLock} className="input-icon" />
-            <input className={errors.password ? "validation-error" : ""}
-            type="password" 
-            id="password"
-            name='password'
-            placeholder='Ваш пароль'
-            onChange={signupHandler}
-            />
-            </div>
-             <div className="form-error">
-                {errors.password && <p>{errors.password}</p>}
-            </div>
-            <button className="login-btn" type='submit'>Войти</button>
-            <div className="form-link">Нет аккаунта? <Link to="#">Зарегистрироваться</Link></div>
+        <form className="login-form" onSubmit={handleSubmit(onSubmit)}>
+          <label className="login-form__label" for="email">Почта</label>
+          <button className="login-btn" type='submit'>Войти</button>
         </form>
         </div>
     </div>
+
+
+
+
+// <div className="main">
+// <div className="login">
+//  <div className="login-sidebar"></div>
+//   <form className="login-form" action="">
+//   <div className="login-form__title">Авторизация</div>
+//       <label className="login-form__label" for="email">Почта</label>
+//       <div className="login-form__input">
+//       <FontAwesomeIcon icon={faEnvelope} className="input-icon" />
+//       <input
+//       type="text" 
+//       id="email" 
+//       name='email'
+//       placeholder='Укажите свою почту'
+//       />
+//       </div>
+//        <div className="form-error">
+        
+//       </div>
+//       <label className="login-form__label" for="password">Пароль</label>
+//       <div className="login-form__input">
+//       <FontAwesomeIcon icon={faLock} className="input-icon" />
+//       <input
+//       type="password" 
+//       id="password"
+//       name='password'
+//       placeholder='Ваш пароль'
+//       />
+//       </div>
+//        <div className="form-error">
+          
+//       </div>
+//       <button className="login-btn" type='submit'>Войти</button>
+//       <div className="form-link">Нет аккаунта? <Link to="#">Зарегистрироваться</Link></div>
+//   </form>
+//   </div>
+// </div>
   )
 };
--- client/src/components/form/Signup.jsx
+++ client/src/components/form/Signup.jsx
@@ -1,72 +1,130 @@
-import React from 'react';
-import { Link } from "react-router-dom";
+import React, {useRef} from 'react';
 import "./Login.css";
-import useSignup from '../../hooks/useSignup';
-import validateAuth from '../validate/validateAuth.js'
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { faEnvelope, faLock } from '@fortawesome/free-solid-svg-icons';
+import {useForm} from 'react-hook-form';
 
 export default function Signup() {
-    
-    const {signupHandler, signup, errors, signupData} = useSignup(validateAuth);
 
+  const {
+    register,
+    formState: {errors},
+    handleSubmit,
+    watch,
+  } = useForm();
+
+  const password = useRef({});
+  password.current = watch("password", "")
+
+  const onSubmit = (data) => {
+    console.log(data)
+  }
 
   return (
     <div className="main">
         <div className="login">
         <div className="signup-sidebar"></div>
-            <form className="login-form" action="" onSubmit={signup}>
-            <div className="login-form__title">Регистрация</div>
-                <label className="login-form__label" for='email'>Почта</label>
-                <div className="login-form__input">
-                <FontAwesomeIcon icon={faEnvelope} className="input-icon" />
-                <input className={errors.email ? "validation-error" : ""}
-                type='text' 
-                id='email'
-                name='email'
-                placeholder='Укажите свою почту'
-                onChange={signupHandler}
-                value={signupData.email}
-                />
-                </div>
-                <div className="login-form__error">
-                    {errors.email && <p>{errors.email}</p>}
-                </div>
-                <label className="login-form__label" for="password">Пароль</label>
-                <div className="login-form__input">
-                <FontAwesomeIcon icon={faLock} className="input-icon" />
-                <input className={errors.password ? "validation-error" : ""}
-                type='password'
-                id='password'
-                name='password'
-                placeholder='Ваш пароль'
-                onChange={signupHandler}
-                value={signupData.password}
-                />
-                </div>
-                <div className="login-form__error">
-                    {errors.password && <p>{errors.password}</p>}
-                </div>
-                <label className="login-form__label" for="confirmPassword">Подтвердите пароль</label>
-                <div className="login-form__input">
-                <FontAwesomeIcon icon={faLock} className="input-icon" />
-                <input className={errors.confirmPassword ? "validation-error" : ""}
-                type='password'
-                id='confirmPassword'
-                name='confirmPassword'
-                placeholder='Повторите пароль'
-                onChange={signupHandler}
-                value={signupData.confirmPassword}
-                />
-                </div>
-                <div className="login-form__error">
-                {errors.confirmPassword && <p>{errors.confirmPassword}</p>}
-                </div>
-                <div className="restore-password"><Link to="#">Забыли пароль?</Link></div>
-                <button className="login-btn" type='submit'>Отправить</button>
-                <div className="form-link ">Уже есть аккаунт? <Link to="#">Войти</Link></div>
-            </form>
+        <form className="login-form" onSubmit={handleSubmit(onSubmit)}>
+        <div className="login-form__title">Регистрация</div>
+        <label className="login-form__label" for="email">
+          <div className="login-form__input">
+          <FontAwesomeIcon icon={faEnvelope} className="input-icon" />
+          <input placeholder="Почта"
+          {...register("email", {
+          required: "Please, add your email",
+          })}
+       />
+        <div className="form-error">
+          {errors?.email && <p>{errors?.email?.message || "Error"}</p>}
+        </div>
+       </div>
+        </label>
+        <label className="login-form__label" for="password">
+          <div className="login-form__input">
+          <FontAwesomeIcon icon={faLock} className="input-icon" />
+          <input 
+          placeholder="Пароль"
+          type="password"
+          {...register("password", {
+          required: "Укажите, пожалуйста, пароль",
+          })}
+       />
+        <div className="form-error">
+          {errors?.password && <p>{errors?.password?.message || "Error"}</p>}
+        </div>
+       </div>
+        </label>
+        <label className="login-form__label" for="confirmPassword">
+          <div className="login-form__input">
+          <FontAwesomeIcon icon={faLock} className="input-icon" />
+          <input 
+          placeholder="Повторите пароль"
+          type="password"
+          {...register("confirmPassword", {
+          required: "Укажите, пожалуйста, пароль",
+          validate:value => value === password.current || "Пароли не совпадают"
+          })}
+       />
+        <div className="form-error">
+          {errors?.confirmPassword && <p>{errors?.confirmPassword?.message || "Error"}</p>}
+        </div>
+       </div>
+        </label>
+        <button className="login-btn" type='submit'>Войти</button>
+        </form> 
         </div>
     </div>
+
+
+
+
+// <div className="main">
+// <div className="login">
+// <div className="signup-sidebar"></div>
+//     <form className="login-form" action="">
+//     <div className="login-form__title">Регистрация</div>
+//         <label className="login-form__label" for='email'>Почта</label>
+//         <div className="login-form__input">
+//         <FontAwesomeIcon icon={faEnvelope} className="input-icon" />
+//         <input
+//         type='text' 
+//         id='email'
+//         name='email'
+//         placeholder='Укажите свою почту'
+//         />
+//         </div>
+//         <div className="login-form__error">
+//         </div>
+//         <label className="login-form__label" for="password">Пароль</label>
+//         <div className="login-form__input">
+//         <FontAwesomeIcon icon={faLock} className="input-icon" />
+//         <input
+//         type='password'
+//         id='password'
+//         name='password'
+//         placeholder='Ваш пароль'
+//         />
+//         </div>
+//         <div className="login-form__error">
+          
+//         </div>
+//         <label className="login-form__label" for="confirmPassword">Подтвердите пароль</label>
+//         <div className="login-form__input">
+//         <FontAwesomeIcon icon={faLock} className="input-icon" />
+//         <input
+//         type='password'
+//         id='confirmPassword'
+//         name='confirmPassword'
+//         placeholder='Повторите пароль'
+//         />
+//         </div>
+//         <div className="login-form__error">
+//         </div>
+//         <div className="restore-password"><Link to="#">Забыли пароль?</Link></div>
+//         <button className="login-btn" type='submit'>Отправить</button>
+//         <div className="form-link ">Уже есть аккаунт? <Link to="#">Войти</Link></div>
+//     </form>
+// </div>
+// </div>
   )
 };
